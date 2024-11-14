7f5f7f8162d0da13c58a6c489d86b40c76eeadc7
Mostafa12248 <mostafayahya000@gmail.com>
2024-11-14 08:00:32 +0200

    finished the project

--- app/script/script.js
+++ app/script/script.js
@@ -1,7 +1,29 @@
 const box = document.querySelectorAll('.main__boxes .box');
-
 box.forEach((ele) => {
   ele.addEventListener('click', () => {
     ele.classList.toggle('border-left');
   });
+});
+
+function search() {
+  let searchInput = document.querySelector('#search');
+  box.forEach((ele) => {
+    ele
+      .querySelectorAll('.skill')
+      .forEach((ele1) =>
+        ele1.innerHTML.includes(searchInput.value)
+          ? ele.classList.add('grid')
+          : ele.classList.add('none')
+      );
+  });
+  searchInput.value = '';
+}
+
+const searchBtn = document.querySelector('button');
+searchBtn.addEventListener('click', () => {
+  search();
+});
+
+searchBtn.addEventListener('keydown', (e) => {
+  e.key === 'Enter' ? search() : false;
 });
